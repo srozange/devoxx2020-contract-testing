fd37cfef785ed2047389c89aaa3d92977657523c
Maxime GELLE <maxime.gelle@gmail.com>
2022-04-19 11:07:03 +0200

    Migrate to java 11+

--- checkout/src/main/java/com/devoxx/checkout/CheckoutApplication.java
+++ checkout/src/main/java/com/devoxx/checkout/CheckoutApplication.java
@@ -1,10 +1,8 @@
 package com.devoxx.checkout;
 
-import com.devoxx.checkout.provider.Streams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
@@ -19,32 +17,31 @@ import java.io.IOException;
 import static java.util.Collections.singletonList;
 
 @SpringBootApplication
-@EnableBinding(Streams.class)
 public class CheckoutApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CheckoutApplication.class, args);
     }
 
-//    class JsonRequestInterceptor implements ClientHttpRequestInterceptor {
-//
-//        @Override
-//        public ClientHttpResponse intercept(HttpRequest request, byte[] body,
-//                                            ClientHttpRequestExecution execution) throws IOException {
-//
-//            HttpRequestWrapper requestWrapper = new HttpRequestWrapper(request);
-//
-//            requestWrapper.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-//            requestWrapper.getHeaders().setAccept(singletonList(MediaType.APPLICATION_JSON));
-//
-//            return execution.execute(requestWrapper, body);
-//        }
-//    }
+    static class JsonRequestInterceptor implements ClientHttpRequestInterceptor {
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest request, byte[] body,
+                                            ClientHttpRequestExecution execution) throws IOException {
+
+            HttpRequestWrapper requestWrapper = new HttpRequestWrapper(request);
+
+            requestWrapper.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+            requestWrapper.getHeaders().setAccept(singletonList(MediaType.APPLICATION_JSON));
+
+            return execution.execute(requestWrapper, body);
+        }
+    }
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         RestTemplate restTemplate = builder.build();
-        //restTemplate.setInterceptors(singletonList(new JsonRequestInterceptor()));
+        restTemplate.setInterceptors(singletonList(new JsonRequestInterceptor()));
         return restTemplate;
     }
 
